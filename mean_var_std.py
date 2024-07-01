0b6411c12364c1a039b1d9bfcdae34de54f2447f
Goddii Asewe <otienogodwin23@gmail.com>
2024-07-01 01:54:27 +0000

    final code

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,25 +1,30 @@
 import numpy as np
 import pprint
 
-def calculate(list):
+def calculate(input_list):
 
-    if len(list) != 9:
+    if len(input_list) != 9:
         raise ValueError('List must contain nine numbers')
 
-    list_num = np.array(list)
+    list_num = np.array(input_list)
     matrix_list = list_num.reshape(3,3)
-    flatten_list = matrix_list.flatten()
-    mean ,variance, standard, max_value, min_value =  matrix_list.mean(axis=0), matrix_list.var(axis=0),matrix_list.std(axis=0),matrix_list.max(axis=0),matrix_list.min(axis=0)
-    mean_num ,variance_num, standard_num, max_value_num, min_value_num =  matrix_list.mean(axis=1), matrix_list.var(axis=1),matrix_list.std(axis=1),matrix_list.max(axis=1),matrix_list.min(axis=1)
-    mean_list ,variance_list, standard_list, max_value_list, min_value_list =  flatten_list.mean(), flatten_list.var(),flatten_list.std(),flatten_list.max(),flatten_list.min()
+
+    mean = [list(np.mean(matrix_list, axis= 0)), list(np.mean(matrix_list, axis=1)), np.mean(matrix_list)]
+    variance_value = [list(np.var(matrix_list, axis=0)), list(np.var(matrix_list, axis=1)), np.var(matrix_list)]
+    standard_value = [list(np.std(matrix_list, axis= 0)), list(np.std(matrix_list, axis=1)), np.std(matrix_list)]
+    max_values = [list(np.max(matrix_list, axis= 0)), list(np.max(matrix_list, axis=1)), np.max(matrix_list)]
+    min_values = [list(np.min(matrix_list, axis= 0)), list(np.min(matrix_list, axis=1)), np.min(matrix_list)]
+    sum_values = [list(np.sum(matrix_list, axis= 0)), list(np.sum(matrix_list, axis=1)), np.sum(matrix_list)]
 
     calculations = {
-        'Mean' : [mean.tolist(), mean_num.tolist(), mean_list.tolist()],
-        'Variance' : [variance.tolist(), variance_num.tolist(),variance_list.tolist()],
-        'Standard deviation' : [standard.tolist(), standard_num.tolist(), standard_list.tolist()],
-        'Max' : [max_value.tolist(), max_value_num.tolist(), max_value_list.tolist()],
-        'Min' : [min_value.tolist(), min_value_num.tolist(), min_value_list.tolist()]
+        'Mean': mean,
+        'Variance' : variance_value,
+        'Standard_deviation' : standard_value,
+        'Max' : max_values,
+        'Min' : min_values,
+        'Sum' : sum_values
     }
+
 
     return calculations
 
